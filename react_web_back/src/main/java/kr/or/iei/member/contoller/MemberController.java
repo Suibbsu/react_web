e705370d68a1379b465296500c4df053cf4d49fd
Suibbsu <142184204+Suibbsu@users.noreply.github.com>
2023-09-15 16:00:28 +0900

    Initial commit

---
+++ react_web_back/src/main/java/kr/or/iei/member/contoller/MemberController.java
@@ -0,0 +1,11 @@
+package kr.or.iei.member.contoller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/member")
+public class MemberController {
+
+}
